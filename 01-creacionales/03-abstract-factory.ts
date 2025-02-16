f81d0ab657aecfa780534fb6ea56a627dcedf262
Nisanech <57728976+Nisanech@users.noreply.github.com>
2025-02-15 21:11:52 -0500

    Abstract factory design pattern

--- 01-creacionales/03-abstract-factory.ts
+++ 01-creacionales/03-abstract-factory.ts
@@ -12,8 +12,88 @@
  * https://refactoring.guru/es/design-patterns/abstract-factory
  */
 
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  *  El propósito del Abstract Factory es crear familias de objetos relacionados
  *  (en este caso, hamburguesas y bebidas) sin especificar las clases concretas
  *  de cada uno de esos objetos en el código principal.
  */
+
+interface Hamburger {
+  prepare(): void;
+}
+
+interface Drink {
+  pour(): void;
+}
+
+// Products
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cChicken burger", COLORS.yellow);
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing %cBeef burger", COLORS.red);
+  }
+}
+
+class Water implements Drink {
+  pour(): void {
+    console.log("Pouring a glass of %cWater", COLORS.blue);
+  }
+}
+
+class Soda implements Drink {
+  pour(): void {
+    console.log("Pouring a glass of %cSoda", COLORS.pink);
+  }
+}
+
+// Factories
+
+interface RestaurantFactory {
+  createHamburger(): Hamburger;
+  createDrink(): Drink;
+}
+
+class FastFoodRestaurantFactory implements RestaurantFactory {
+  createDrink(): Drink {
+    return new Soda();
+  }
+
+  createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+}
+
+class HealthyFoodRestaurantFactory implements RestaurantFactory {
+  createDrink(): Drink {
+    return new Water();
+  }
+
+  createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+}
+
+// Implemented pattern
+
+function main(factory: RestaurantFactory) {
+  const hamburger = factory.createHamburger();
+  const drink = factory.createDrink();
+
+  hamburger.prepare();
+
+  drink.pour();
+}
+
+console.log("\n%cOrder from the regular menu", COLORS.green);
+main(new FastFoodRestaurantFactory());
+
+console.log("\n\n%cOrder from the healthy menu", COLORS.green);
+main(new HealthyFoodRestaurantFactory())
