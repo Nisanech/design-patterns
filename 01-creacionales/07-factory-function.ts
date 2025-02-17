7ea42f79ab559b3af433931c8e2955b3d931d9da
Nisanech <57728976+Nisanech@users.noreply.github.com>
2025-02-16 20:28:27 -0500

    Factory function design pattern

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -1,3 +1,5 @@
+import {COLORS} from "../helpers/colors.ts";
+
 /**
  * ! Factory Function
  * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
@@ -7,3 +9,29 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+type Language = 'es' | 'en' | 'fr'
+
+//* i18n
+function createGreeter(lang: Language) {
+  return function (name: string) {
+    const messages = {
+      es: `Hola, %c${ name }!`,
+      en: `Hello, %c${ name }!`,
+      fr: `Bonjour, %c${ name }!`,
+    }
+    
+    return console.log(messages[lang], COLORS.red)
+  }
+}
+
+function main() {
+  const spanishGreeter = createGreeter('es')
+  const englishGreeter = createGreeter('en')
+  const frenchGreeter = createGreeter('fr')
+  
+  spanishGreeter('Nicolas')
+  englishGreeter('Diana')
+  frenchGreeter('Josue')
+}
+
+main();
